259da907ac7b4abda233d174f0f6154f545881ab
thomas-is <thomas@0l.fi>
2021-02-03 08:42:36 +0100

    :tada: initial commit

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Yes/No/All
+OVERWRITE=""
+
+for RC in bashrc vimrc
+do
+
+  if [ -f ~/.$RC ] ; then
+    if [ "$OVERWRITE" != "a" ] ; then
+      printf "overwrite ~/.$RC ? [y]es [n]o [a]ll e[x]it "
+      read -n 1 OVERWRITE
+      OVERWRITE=$( echo $OVERWRITE | tr '[:upper:]' '[:lower:]' )
+      printf "\n"
+      if [ "$OVERWRITE" = "x" ] ; then
+        exit 1
+      fi
+    fi
+    if [ "$OVERWRITE" = "y" ] || [ "$OVERWRITE" = "a" ] ; then
+      echo "rm ~/.$RC"
+      rm ~/.$RC
+    else
+      continue
+    fi
+  fi
+
+  echo "ln -s $(pwd)/$RC ~/.$RC"
+  ln -s $(pwd)/$RC ~/.$RC
+
+done
+
