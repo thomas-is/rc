bbc75b5df7bf6ff91a2789093eac219e47c979af
thomas-is <thomas@0l.fi>
2021-06-25 05:49:41 +0200

    :art: improve

--- colors.sh
+++ colors.sh
@@ -1,25 +1,58 @@
-#!/bin/sh
+#!/bin/bash
+
+
+
+CSI() { printf "\033["; }
+SGR() { CSI; printf "${1:-0}m"; }
+
+CUU() { CSI; printf "${1:-1}a"; }
+
+BOLD()   { CSI; SGR "1"; }
+#FG_RGB() { CSI; SGR "38;2;${1:-255};${2:-255};${3:-255}"; echo "$1 $2 $3"; }
+BG_RGB() { CSI; SGR "48;2;${1:-255};${2:-255};${3:-255}"; }
+
+FG() { CSI; SGR "38;5;${1:-0}"; }
+BG() { CSI; SGR "48;5;${1:-0}"; }
 
 RST="\033[0m\033m"
-RED="\033[0;31m\033m"
-GRN="\033[0;32m\033m"
-ORG="\033[0;33m\033m"
-BLU="\033[0;34m\033m"
+RED="\033[31m\033m"
+GRN="\033[32m\033m"
+ORG="\033[33m\033m"
+BLU="\033[34m\033m"
 
-color() {
-  printf "\033[${2:-0};${1:-0}m\033m"
+render() {
+  BG $1
+  printf " %3d " $1
+  printf "$RST"
 }
 
-for I in 0 1 2 3 4 5 6 7
+printf "$RST"
+
+for line in {0..1}
 do
-  color; printf "3$I "
-  for J in 0 1 2 3 4 5 6 7 8 9 10 11 12
+  for col in {0..7}
   do
-    color 3$I $J
-    printf "$J"
-    color
-    printf " "
+    let N=$line*8+$col
+    render $N
   done
-  echo
+  printf "\n"
 done
-color
+printf "\n"
+
+for line in {0..5}
+do
+  for col in {16..51}
+  do
+    let N=$line*36+$col
+    render $N
+  done
+  printf "\n"
+done
+printf "\n"
+
+for N in {232..255}
+do
+  render $N
+done
+printf "\n"
+printf "$RST"
